8841145a4128b01e0d7bc87b2b5c116bd1b2beae
hzg <15934567149@163.com>
2019-03-14 13:29:06 +0800

    完善功能

--- src/js/index.js
+++ src/js/index.js
@@ -16,3 +16,12 @@ var bs = new BScroll('.main', {
     probeType: 2,
     click: true
 });
+
+var list = document.querySelector('.like-list');
+
+axios.get('/api/list').then(function (res) {
+    console.log(res.data.data);
+    list.innerHTML = res.data.data.map(function (val) {
+        return '<li>\n                    <img src="images/eat_03.png" alt="">\n                    <div class="like-cont">\n                        <h3>' + val.name + '</h3>\n                        <p>[15\u5E97\u901A\u7528]\u9E21\u817F\u5821 + \u85AF\u6761 + \u4E2D\u676F\u53EF\u4E50</p>\n                        <div class="price">\n                            <div>\n                                <span>' + val.price + '\u5143</span>\n                                <em>\u95E8\u5E02\u4EF7\uFF1A' + val.door + '\u5143</em>\n                            </div>\n                            <em>\u5DF2\u552E' + val.num + '</em>\n                        </div>\n                    </div>\n                </li>';
+    }).join('');
+});
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -13,4 +13,26 @@ var swiper2 = new Swiper('.right', {
 var bs = new BScroll('.main', {
     probeType: 2,
     click: true
+});
+
+const list = document.querySelector('.like-list');
+
+axios.get('/api/list').then(res => {
+    console.log(res.data.data);
+    list.innerHTML = res.data.data.map((val => {
+        return `<li>
+                    <img src="images/eat_03.png" alt="">
+                    <div class="like-cont">
+                        <h3>${val.name}</h3>
+                        <p>[15店通用]鸡腿堡 + 薯条 + 中杯可乐</p>
+                        <div class="price">
+                            <div>
+                                <span>${val.price}元</span>
+                                <em>门市价：${val.door}元</em>
+                            </div>
+                            <em>已售${val.num}</em>
+                        </div>
+                    </div>
+                </li>`
+    })).join('');
 })
